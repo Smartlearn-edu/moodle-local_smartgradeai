1c25d2d8c00dbe6836f4f3252872a244284f6be2
Smat Learn <113486938+Smartlearn-edu@users.noreply.github.com>
2026-03-07 04:40:52 +0200

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_smartgradeai';
-$plugin->version   = 2026020203; // YYYYMMDDXX - Bumped for Review Table
+$plugin->version   = 2026020205; // YYYYMMDDXX - Bumped for Review Table
 $plugin->requires  = 2022112800; // Requires Moodle 4.1+ (adjust as needed)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '0.1.1';
+$plugin->release   = '1.0.0';
